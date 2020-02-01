e375359d7d5700d464d407454995521d1248a833
gogbajbobo <grigoriev.maxim@gmail.com>
2020-02-01 14:13:15 +0300

    use express

--- src/server.js
+++ src/server.js
@@ -1,5 +1,19 @@
+const
+    express = require('express'),
+    http = require('http')
+
+const app = express()
+const port = 8081
+const server = http.createServer(app)
+
+app.get('/', (req, res) => res.json({ error: false, message: 'SES server ok' }))
+
 const startServer = () => {
-    console.log(`SES server start at ${ new Date() }`)
+
+    server.listen(port, () => {
+        console.log(`SES server started at ${ new Date() } on *:${ port }`)
+    })
+
 }
 
 module.exports = { startServer }
