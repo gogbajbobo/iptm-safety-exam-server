8b3f6d3c717d9a85d9940a3723eff8e8b1d2e485
gogbajbobo <grigoriev.maxim@gmail.com>
2020-02-05 13:45:36 +0300

    socket events as constants

---
+++ src/socket/events.js
@@ -0,0 +1,13 @@
+const SOCKET = {
+
+    CONNECTION: 'connection',
+    ERROR: 'error',
+    DISCONNECT: 'disconnect',
+    DISCONNECTING: 'disconnecting',
+
+    MESSAGE: 'message',
+    LOGIN: 'login',
+
+}
+
+module.exports = { SOCKET }
--- src/socket/index.js
+++ src/socket/index.js
@@ -1,4 +1,5 @@
 const socketIO = require('socket.io')
+const { SOCKET } = require('../socket/events')
 const auth = require('../services/auth')
 
 
@@ -6,30 +7,30 @@ const initSocket = http => {
 
     const io = socketIO(http)
 
-    io.on('connection', socket => {
+    io.on(SOCKET.CONNECTION, socket => {
 
         console.log(`socket ${ socket.id } connected`)
 
-        socket.on('disconnect', reason => {
+        socket.on(SOCKET.DISCONNECT, reason => {
             console.log(`socket ${ socket.id } disconnected with reason: ${ reason }`)
         })
 
-        socket.on('error', error => {
+        socket.on(SOCKET.ERROR, error => {
             console.error(`socket ${ socket.id } error: ${ error }`)
         })
 
-        socket.on('disconnecting', reason => {
+        socket.on(SOCKET.DISCONNECTING, reason => {
             console.log(`socket ${ socket.id } disconnecting with reason: ${ reason }`)
         })
 
-        socket.on('message', message => {
+        socket.on(SOCKET.MESSAGE, message => {
 
             console.log(`socket ${ socket.id } message: ${ message }`)
             io.emit('message', message)
 
         })
         socket.on(
-            'login',
+            SOCKET.LOGIN,
             (data, callback) => {
 
                 console.log(`socket ${ socket.id } login: ${ JSON.stringify(data, null, '\t') }`)
