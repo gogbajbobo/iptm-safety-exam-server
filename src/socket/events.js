08fc7bc1a193564b1ec1ba9331016a791e15f947
gogbajbobo <grigoriev.maxim@gmail.com>
2020-02-06 14:55:00 +0300

    extract listenEvents to socket/events.js

--- src/socket/events.js
+++ src/socket/events.js
@@ -10,4 +10,27 @@ const SOCKET = {
 
 }
 
-module.exports = { SOCKET }
+const listenEvents = socket => {
+
+    socket.on(SOCKET.DISCONNECT, reason => {
+        console.log(`socket ${ socket.id } disconnected with reason: ${ reason }`)
+    })
+
+    socket.on(SOCKET.ERROR, error => {
+        console.error(`socket ${ socket.id } error: ${ error }`)
+    })
+
+    socket.on(SOCKET.DISCONNECTING, reason => {
+        console.log(`socket ${ socket.id } disconnecting with reason: ${ reason }`)
+    })
+
+    socket.on(SOCKET.MESSAGE, message => {
+
+        console.log(`socket ${ socket.id } message: ${ message }`)
+        io.emit('message', message)
+
+    })
+
+}
+
+module.exports = { SOCKET, listenEvents }
--- src/socket/index.js
+++ src/socket/index.js
@@ -1,6 +1,6 @@
 const socketIO = require('socket.io')
-const { SOCKET } = require('../socket/events')
 const auth = require('../services/auth')
+    { SOCKET, listenEvents } = require('../socket/events'),
 
 
 const initSocket = http => {
@@ -11,24 +11,11 @@ const initSocket = http => {
 
         console.log(`socket ${ socket.id } connected`)
 
-        socket.on(SOCKET.DISCONNECT, reason => {
-            console.log(`socket ${ socket.id } disconnected with reason: ${ reason }`)
-        })
 
-        socket.on(SOCKET.ERROR, error => {
-            console.error(`socket ${ socket.id } error: ${ error }`)
-        })
 
-        socket.on(SOCKET.DISCONNECTING, reason => {
-            console.log(`socket ${ socket.id } disconnecting with reason: ${ reason }`)
-        })
 
-        socket.on(SOCKET.MESSAGE, message => {
 
-            console.log(`socket ${ socket.id } message: ${ message }`)
-            io.emit('message', message)
 
-        })
         socket.on(
             SOCKET.LOGIN,
             (data, callback) => {
@@ -43,6 +30,7 @@ const initSocket = http => {
 
             }
         )
+        listenEvents(socket)
 
     })
 
