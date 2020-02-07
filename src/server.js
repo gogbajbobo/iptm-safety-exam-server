d4c3419a9f3e03a6f774fd3ef0e719b342d911b6
gogbajbobo <grigoriev.maxim@gmail.com>
2020-02-07 14:36:42 +0300

    Create logger.js

--- src/server.js
+++ src/server.js
@@ -1,19 +1,22 @@
 const
     app = require('express')(),
     http = require('http').createServer(app),
-    socket = require('./socket')
+    socket = require('./socket'),
+    { requestLoggers, log } = require('./services/logger')
+
 
 const port = 8081
 
 app.get('/', (req, res) => res.json({ error: false, message: 'SES server ok' }))
+requestLoggers.forEach(logger => app.use(logger))
 
 const startServer = connection => {
 
-    if (!connection) console.error('Have no connection to database')
+    if (!connection) log.error('Have no connection to database')
 
     http.listen(port, () => {
 
-        console.log(`SES server started at ${ new Date() } on *:${ port }`)
+        log.info(`SES server started at ${ new Date() } on *:${ port }`)
         socket.initSocket(http)
 
     })
