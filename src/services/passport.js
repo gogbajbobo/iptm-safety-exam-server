6ffd2642ce3d170e85a69d3b965fbdd52436c236
gogbajbobo <grigoriev.maxim@gmail.com>
2020-03-06 17:01:15 +0300

    added CookieStrategy

--- src/services/passport.js
+++ src/services/passport.js
@@ -1,15 +1,29 @@
 const
     passport = require('passport'),
     passportLocal = require('passport-local'),
-    UserController = require('../controllers/user')
+    passportCookie = require('passport-cookie'),
+    UserController = require('../controllers/user'),
+    { checkJWT } = require('./auth')
 
 
 const LocalStrategy = passportLocal.Strategy
+const CookieStrategy = passportCookie.Strategy
 
 passport.use(new LocalStrategy((username, password, done) => {
 
     const user = UserController.authUser({ username, password })
     user ? done(null, user) : done(null, false, { message: 'auth fail' })
+
+}))
+
+passport.use(new CookieStrategy({ cookieName: 'authJWT' }, (token, done) => {
+
+    const user = checkJWT(token)
+
+    if (!user)
+        return done(null, false)
+
+    return done(null, user)
 
 }))
 
