79b9c4c34c14eff817790ab77e24926f61619fbd
gogbajbobo <grigoriev.maxim@gmail.com>
2020-03-06 17:00:17 +0300

    use log instead of console

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const db = require('./src/database')
 const server = require('./src/server')
+const { log } = require('./src/services/logger')
 require('reflect-metadata')
 
 
 db.connectDatabase()
     .then(server.startServer)
-    .catch(console.error)
+    .catch(log.error)
