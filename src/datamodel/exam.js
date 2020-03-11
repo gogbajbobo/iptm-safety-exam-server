af6555e99b7f8f1cb451f7d578d8ff909d1aa52d
gogbajbobo <grigoriev.maxim@gmail.com>
2020-03-11 14:40:47 +0300

    added eslint rules

--- src/datamodel/exam.js
+++ src/datamodel/exam.js
@@ -1,11 +1,13 @@
 const EntitySchema = require('typeorm').EntitySchema
 
+/* eslint-disable fp/no-mutation,fp/no-this */
 class Exam {
     constructor(id, title) {
         this.id = id
         this.title = title
     }
 }
+/* eslint-enable fp/no-mutation,fp/no-this */
 
 const Schema = new EntitySchema ({
     name: Exam.name,
