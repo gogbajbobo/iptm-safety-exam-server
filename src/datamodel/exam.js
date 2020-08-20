ef8843ba4b7384d26c067005efc14224a91b58d9
gogbajbobo <grigoriev.maxim@gmail.com>
2020-08-20 14:54:45 +0300

    Update exam.js
    rm class Exam {}

--- src/datamodel/exam.js
+++ src/datamodel/exam.js
@@ -1,21 +1,14 @@
 const EntitySchema = require('typeorm').EntitySchema
 
-/* eslint-disable fp/no-mutation,fp/no-this */
-class Exam {
-    constructor(id, title) {
-        this.id = id
-        this.title = title
-    }
-}
-/* eslint-enable fp/no-mutation,fp/no-this */
+const Model = 'Exam'
 
 const Schema = new EntitySchema ({
-    name: Exam.name,
-    target: Exam,
+    name: Model,
+    target: Model,
     columns: {
         id: { type: Number, primary: true, generated: true },
         title: { type: String }
     }
 })
 
-module.exports = { Model: Exam, Schema }
+module.exports = { Model, Schema }
