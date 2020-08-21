602d1663f01a84f9823909b048346c1701ef1d08
gogbajbobo <grigoriev.maxim@gmail.com>
2020-08-21 16:53:13 +0300

    Create _helper.js

---
+++ src/controllers/_helper.js
@@ -0,0 +1,9 @@
+const requestHandler = (requestPromise, ack) => {
+
+    return requestPromise
+        .then(payload => ack({ payload }))
+        .catch(error => ack({ error }))
+
+}
+
+module.exports = { requestHandler }
