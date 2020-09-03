7c796c171654eb1ebbb9b0bec3d34d1f4cba866b
gogbajbobo <grigoriev.maxim@gmail.com>
2020-09-03 15:33:21 +0300

    return questions with answers

--- src/controllers/question.js
+++ src/controllers/question.js
@@ -1,40 +1,65 @@
 const typeorm = require('typeorm')
 const Question = require('../datamodel/_model_names').question
+const Answer = require('../datamodel/_model_names').answer
 const { requestHandler } = require('./_helper')
 const userRoles = require('../datamodel/_user_roles')
 const { CONNECTION_NAME } = require('../constants')
 
 
 const QuestionRepository = () => typeorm.getRepository(Question, CONNECTION_NAME)
+const AnswerRepository = () => typeorm.getRepository(Answer, CONNECTION_NAME)
 
 const createQuestion = (question, ack) => requestHandler(() => QuestionRepository().save(question), ack)
 
-const getQuestions = ({ role, filter }, ack) => requestHandler(() => {
+const getQuestions = ({ role, exam }, ack) => requestHandler(() => {
 
     if (!role)
         return Promise.reject(new Error('have no role in request'))
 
     if (role === userRoles.admin)
-        return QuestionRepository().find(filter)
+        return QuestionRepository().find({ exam })
 
-    if (role === userRoles.examinee) {
-
-        const findFilter = { where: filter, order: 'RAND()', take: 10 }
-
-        return QuestionRepository().createQueryBuilder()
-            .where(`examId = :exam`, filter)
-            .orderBy('RAND()')
-            .limit(10)
-            .getMany()
-
-        // http://jan.kneschke.de/projects/mysql/order-by-rand/
-
-    }
-
+    if (role === userRoles.examinee)
+        return getQuestionsToTakeExam({ exam })
 
     return Promise.reject(new Error('incomplete request'))
 
 }, ack)
+
+const getQuestionsToTakeExam = examFilter => {
+
+    // http://jan.kneschke.de/projects/mysql/order-by-rand/
+    return QuestionRepository().createQueryBuilder()
+        .where(`examId = :exam`, examFilter)
+        .orderBy('RAND()')
+        .limit(10)
+        .getMany()
+        .then(questions => {
+
+            const questionsIds = questions.map(q => q.id)
+            return AnswerRepository().createQueryBuilder()
+                .where('questionId IN (:...questionsIds)', { questionsIds })
+                .getMany()
+                .then(answers => {
+
+                    return questions.map(q => {
+
+                        const filteredAnswers = answers.filter(ba => ba['questionId'] === q.id)
+                        return { ...q, answers: blankedAnswers(filteredAnswers) }
+
+                    })
+
+                })
+
+        })
+
+}
+
+const blankedAnswers = answers => {
+    return answers.map(a => {
+        return { ...a, isCorrect: undefined, questionId: undefined }
+    })
+}
 
 const updateQuestion = (question, ack) => requestHandler(() => QuestionRepository().save(question), ack)
 
