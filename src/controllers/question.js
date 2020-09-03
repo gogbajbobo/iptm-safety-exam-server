6e83f506ba6b233cc2081cfcad1d8acb8679afbf
gogbajbobo <grigoriev.maxim@gmail.com>
2020-09-03 12:46:54 +0300

    find request depends on role

--- src/controllers/question.js
+++ src/controllers/question.js
@@ -1,6 +1,7 @@
 const typeorm = require('typeorm')
 const Question = require('../datamodel/_model_names').question
 const { requestHandler } = require('./_helper')
+const userRoles = require('../datamodel/_user_roles')
 
 
 const QuestionRepository = () => typeorm.getRepository(Question, 'iptm-ses-db')
@@ -9,8 +10,17 @@ const createQuestion = (question, ack) => requestHandler(() => QuestionRepositor
 
 const getQuestions = ({ role, filter }, ack) => requestHandler(() => {
 
+    if (!role)
+        return Promise.reject(new Error('have no role in request'))
 
+    if (role === userRoles.admin)
         return QuestionRepository().find(filter)
+
+    if (role === userRoles.examinee)
+        return QuestionRepository().find(filter).orderBy('RAND()').limit(10)
+
+    return Promise.reject(new Error('incomplete request'))
+
 }, ack)
 
 const updateQuestion = (question, ack) => requestHandler(() => QuestionRepository().save(question), ack)
